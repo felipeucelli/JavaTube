12f20c6548bc113161496ffcecedf0295a7fbc7c
Felipe Ucelli <78266261+felipeucelli@users.noreply.github.com>
2023-02-06 20:05:12 -0400

    Used regex to extract data from subtitles xml

--- src/main/java/javatube/Captions.java
+++ src/main/java/javatube/Captions.java
@@ -3,13 +3,13 @@ package javatube;
 import org.json.JSONObject;
 
 import java.io.IOException;
+import java.net.URLDecoder;
 import java.nio.charset.StandardCharsets;
 import java.nio.file.Files;
 import java.nio.file.Path;
 import java.nio.file.Paths;
-import java.util.ArrayList;
-import java.util.Arrays;
-import java.util.List;
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
 
 
 public class Captions {
@@ -17,18 +17,21 @@ public class Captions {
     public String url;
     public String code;
 
-    public Captions(JSONObject captionTrack) throws IOException {
+    public Captions(JSONObject captionTrack){
         url = captionTrack.getString("baseUrl");
         code = captionTrack.getString("vssId").replace(".", "");
-
     }
 
     private String getXmlCaptions() throws IOException {
-        return InnerTube.downloadWebPage(url).replace("&#39;", "'");
+        return InnerTube.downloadWebPage(url).replaceAll("(&#39;)|(&amp;#39;)", "'");
     }
 
     private String generateSrtCaptions() throws IOException {
         return getXmlCaptions();
+    }
+
+    private String decodeString(String s){
+        return URLDecoder.decode(s, StandardCharsets.UTF_8);
     }
 
     private String srtTimeFormat(Float d){
@@ -41,27 +44,30 @@ public class Captions {
         return String.format("%02d:%02d:%02d,", hours, minutes, seconds) + String.format("%.3f", ms).replace("0,", "");
     }
 
-    public String xmlCaptionToSrt() throws IOException {
+    public String xmlCaptionToSrt() throws Exception {
         String root = generateSrtCaptions();
-        String initialBody = new ArrayList<>(List.of((root.split("<body>")))).get(1);
-        String fullBody = new ArrayList<>(List.of((initialBody.split("</body>")))).get(0);
-        ArrayList<String> obj = new ArrayList<>(List.of((fullBody.replace("</p>", "\n").replace("<p", "")).split("\n")));
 
-        String segments = "";
         int i = 0;
-        for(String child : obj){
-            String caption = Arrays.asList(child.split(">")).get(1);
-            Float start = Float.parseFloat((new ArrayList<>(List.of(child.split("d=\"[0-9]*\""))).get(0).replaceAll("[t=\"]", "").replace(" ", ""))) / 1000.0F;
-            Float duration = Float.parseFloat((new ArrayList<>(List.of(child.split("t=\"[0-9]*\""))).get(1).replaceAll("[d=\"]", "").replaceAll(">.*", "").replace(" ", ""))) / 1000.0F;
+        String segments = "";
+
+        String pattern = "start=\\\"(.*?)\\\".*?dur=\\\"(.*?)\\\">(.*?)<";
+        Pattern regex = Pattern.compile(pattern);
+        Matcher matcher = regex.matcher(root);
+        while (matcher.find()) {
+            Float start = Float.parseFloat(matcher.group(1));
+            Float duration = Float.parseFloat(matcher.group(2));
+            String caption = decodeString(matcher.group(3));
+
             Float end = start + duration;
             int sequenceNumber = i + 1;
 
-            String line = sequenceNumber + "\n" + srtTimeFormat(start) + " --> " + srtTimeFormat(end) + "\n" + caption + "\n";
+            String line = sequenceNumber + "\n" + srtTimeFormat(start) + " --> " + srtTimeFormat(end) + "\n" + caption + "\n\n";
 
             segments += line;
 
             i++;
         }
+
         return segments;
     }
 
@@ -82,6 +88,8 @@ public class Captions {
             }
             catch (IOException ex) {
                 System.out.print("Invalid Path");
+            } catch (Exception e) {
+                throw new RuntimeException(e);
             }
         }else{
             try {
